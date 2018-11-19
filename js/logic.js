0d877db426746f626195be609a531ce5a074c744
Kishore Ramakrishnan <38735766+kishore145@users.noreply.github.com>
2018-11-18 23:10:58 -0800

    Added additional Basemaps

--- js/logic.js
+++ js/logic.js
@@ -8,8 +8,27 @@ function createMap(properties){
   accessToken: API_KEY
   });
 
+  googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+    maxZoom: 18,
+    subdomains:['mt0','mt1','mt2','mt3']
+  });
+
+  googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 18,
+    subdomains:['mt0','mt1','mt2','mt3']
+  });
+
+  googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+  });
+
   var baseMaps = {
-  "Base World Map" : lightmap
+  "Base World Map" : lightmap,
+  "Satellite": googleSat,
+  "Google Street" : googleStreets,
+  "Terrain": googleTerrain
+
   };
 
   var overlayMaps = {
